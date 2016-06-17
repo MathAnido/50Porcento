c8eb00b3a3e5327ffcb38aa8ccb7b31512c7592a
MathAnido <matheus_apc2@hotmail.com>
2016-06-17 13:11:23 -0300

    Melhorias

--- server.R
+++ server.R
@@ -1,40 +1,33 @@
-  # By default, the file size limit is 5MB. It can be changed by
-  # setting this option. Here we'll raise limit to 9MB.
-  options(shiny.maxRequestSize = 9*1024^2)
-  
-  shinyServer(function(input, output, session) {
-    csv <- reactive({
-      inFile<-input$file1
-      if(is.null(inFile))
-        return(NULL)
-      read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
-    })
-    opt <- reactive({
-      inFile <- input$file1
-      if(is.null(inFile))
-        return(NULL)
-      read.csv(inFile$datapath, sep=input$sep,nrows = 1,header = TRUE)
-    })
-    output$preVisu <- renderDataTable({
-      if (is.null(csv()))
-        return(NULL)
-      else return(csv())
-    })
-    output$msg <- renderText({
-      if(is.null(csv()))
-        return("Você deve selecionar um arquivo")
-      else return(NULL)
-    })
-    output$msg2 <- renderText({
-      if(is.null(csv()))
-        return("Você deve selecionar um arquivo na Pagina de Upload")
-      else return(NULL)
-    })
-    output$graph1 <- renderPlot({
-      
-    })
-    observe({
-      updateSelectInput(session,"selecao", choices = opt())
-      updateSelectInput(session,"selecao2", choices = opt())
-    })
+options(shiny.maxRequestSize = 9 * 1024 ^ 2)#Tamanho máximo do Arquivo
+
+shinyServer(function(input, output, session) {
+  csv <- reactive({
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+    read.csv(inFile$datapath,header = input$header,sep = input$sep,quote = input$quote)
   })
+  opt <- reactive({
+    if (is.null(csv()))
+      return(NULL)
+    names(csv())
+  })
+  output$preVisu <- renderTable({
+    if (is.null(csv()))
+      return(NULL)
+    else
+      return(csv())
+  })
+  output$msg <- renderText({
+    if (is.null(csv()))
+      return("Você deve selecionar um arquivo")
+  })
+  output$msg2 <- renderText({
+    if (is.null(csv()))
+      return("Você deve selecionar um arquivo na Pagina de Upload")
+  })
+  observe({
+    updateSelectInput(session, "selecao", choices = opt())
+    updateSelectInput(session, "selecao2", choices = opt())
+  })
+})
